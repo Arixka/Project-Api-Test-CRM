58e235241193865e9b5234210c62d5595a4606e1
arixka <marisiver25@gmail.com>
2021-08-30 21:22:32 +0100

    merge

---
+++ src/controllers/upload.controller.js
@@ -0,0 +1,10 @@
+
+
+/**
+ * @route Post /upload/ 
+ * @access Admin User
+ * @returns  
+ */
+ export const uploadFile = async (req, res) => {
+
+ }
---
+++ src/routes/upload.routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { check } from 'express-validator'
+import { validateReq } from '../middlewares/validateReq'
+
+const router = Router()
+
+
+
+
+
+module.exports = router
